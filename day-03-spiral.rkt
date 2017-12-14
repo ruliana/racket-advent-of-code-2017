db4f3694d7c874245db09d21b61540001d7f362f
Ronie Uliana <ronie.uliana@gmail.com>
2017-12-14 09:21:05 -0200

    Add comments and tests
    The logic is not so straight, as I used a closed form solution. So I
    added some comments. Also, I made a lot of tests "by hand" as I was
    trying to understand the best solution, so I added some automated tests
    to be able to refactor it with confidence.

--- day-03-spiral.rkt
+++ day-03-spiral.rkt
@@ -1,31 +1,70 @@
 #lang racket
 
+; Simple x^2 for convenience
 (define (sqr x) (expt x 2))
 
-(define (f x)
-  (+ (* 4 (sqr x)) (* 4 x) 2))
+; "Polar" coordinates for the spiral.
+; The integer part is the distantce from
+; the origin square (1). The fractional
+; part is turn around the spiral, starting
+; from bottom-right.
+; It's used to locate the number without
+; having to fill the spiral.
+(define (coordinates-of x)
+  (add1 (* 1/2 (sub1 (sqrt (sub1 x))))))
 
-(define (invf x)
-  (add1 (* 1/2 (+ -1 (sqrt (sub1 x))))))
-
+; Given a direction, it returns a function
+; that given a layer (distance), returns
+; the number straight on that direction.
+; It's used calculate the "turning point"
+; for the manhattan walker.
 (define ((axis dir) layer)
   (+ (* 4 (sqr layer)) (* dir layer) 1))
 
+; Returns the number on the given layer
+; straight on that direction.
 (define north (axis -1))
 (define east (axis -3))
 (define south (axis 3))
 (define west (axis 1))
 
+; Given a number, what's its layer?
+; It's the distance on my "polar"
+; coordinates.
 (define (layer number)
-  (floor (invf number)))
+  (floor (coordinates-of number)))
 
+; Given a number, returns a function
+; for the direction it is.
 (define (direction number)
-  (define angle (- (invf number) (layer number)))
+  (define angle (- (coordinates-of number) (layer number)))
   (cond
     [(<= angle 0.25) east]
     [(<= 0.25 angle 0.5) north]
     [(<= 0.5 angle 0.75) west]
     [(<= 0.75 angle) south]))
 
+; Calculate how many steps in a
+; manhattan walker I need to reach
+; the number.
 (define (steps-from number)
-  (+ (layer number) (abs (- number ((direction number) (layer number))))))
+  (define deep (layer number))
+  (define straight-number ((direction number) deep))
+  (define lateral-move (abs (- number straight-number)))
+  (inexact->exact (+ deep lateral-move)))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (steps-from 1) 0)
+  (check-equal? (steps-from 2) 1)
+  (check-equal? (steps-from 3) 2)
+  (check-equal? (steps-from 4) 1)
+  (check-equal? (steps-from 5) 2)
+  (check-equal? (steps-from 6) 1)
+  (check-equal? (steps-from 7) 2)
+  (check-equal? (steps-from 8) 1)
+  (check-equal? (steps-from 9) 2)
+  (check-equal? (steps-from 126) 7)
+  (check-equal? (steps-from 127) 6)
+  (check-equal? (steps-from 128) 7)
+  (check-equal? (steps-from 325489) 552))
