a7e5be131e46de07140d043faffc5d5d8965279f
Ronie Uliana <ronie.uliana@gmail.com>
2017-12-14 09:00:13 -0200

    Day 03 - Final
    I used a "closed form" approach. It works beautifully, but I spend a lot
    more time on calculations than on programming.

--- day-03-spiral.rkt
+++ day-03-spiral.rkt
@@ -8,21 +8,24 @@
 (define (invf x)
   (add1 (* 1/2 (+ -1 (sqrt (sub1 x))))))
 
-(define ((axis dir) level)
-  (+ (* 4 (sqr level)) (* dir level) 1))
+(define ((axis dir) layer)
+  (+ (* 4 (sqr layer)) (* dir layer) 1))
 
 (define north (axis -1))
 (define east (axis -3))
 (define south (axis 3))
 (define west (axis 1))
 
-(define (level number)
+(define (layer number)
   (floor (invf number)))
 
 (define (direction number)
-  (define angle (- (invf number) (level number)))
+  (define angle (- (invf number) (layer number)))
   (cond
-    [(<= 0 angle 0.25) east]
+    [(<= angle 0.25) east]
     [(<= 0.25 angle 0.5) north]
     [(<= 0.5 angle 0.75) west]
     [(<= 0.75 angle) south]))
+
+(define (steps-from number)
+  (+ (layer number) (abs (- number ((direction number) (layer number))))))
