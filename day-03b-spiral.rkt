2441a12cf4aed1d7339ba0176155825da4c1e3a5
Ronie Uliana <ronie.uliana@gmail.com>
2017-12-15 18:39:56 -0200

    Day 03 complete
    Using a generator (I like generators) :)

--- day-03b-spiral.rkt
+++ day-03b-spiral.rkt
@@ -1,6 +1,13 @@
 #lang racket
-(require racket/dict)
+(require racket/dict
+         racket/generator)
 
+; Convenience functions
+(define (first seq) (sequence-ref seq 0))
+(define (second seq) (sequence-ref seq 1))
+(define (tail seq) (sequence-tail seq 1))
+
+; Definitions
 (define right 1)
 (define up 0+1i)
 (define left -1)
@@ -16,15 +23,25 @@
 
 (define walk-order (in-cycle (list right up left down)))
 
-(define (fill grid stop-condition pointer direction)
-  (define rslt (for/sum ([pos around]) (dict-ref grid (+ pointer pos) 0)))
-  (define new-grid (dict-set grid pointer rslt))
-  (define new-direction (if (dict-has-key? grid (+ pointer (sequence-ref direction 1)))
-                            (sequence-tail direction 0)
-                            direction))
-  (displayln (sequence-ref new-direction 0))
-  (if (stop-condition rslt)
-      grid
-      (fill new-grid stop-condition (+ pointer (sequence-ref new-direction 0)) new-direction)))
+(define (spiral-sum)
+  (in-generator
+   #:arity 1
+   (yield 1)
+   (let loop ([grid (hash 0+0i 1)]
+              [pointer 0+0i]
+              [direction walk-order])
+     (define look-ahead (+ pointer (second direction)))
+     (define new-direction (if (dict-has-key? grid look-ahead)
+                               direction
+                               (tail direction)))
+     (define new-pointer (+ pointer (first new-direction)))
+     (define rslt (for/sum ([pos around]) (dict-ref grid (+ new-pointer pos) 0)))
+     (define new-grid (dict-set grid new-pointer rslt))
+     (yield rslt)
+     (loop new-grid new-pointer new-direction))))
 
- (fill (hash 0+0i 1) (λ (x) true) 1+0i walk-order)
+(module+ test
+  (require rackunit)
+  (let ([rslt (stop-after (spiral-sum) (λ (x) (>= x 806)))])
+    (check-equal? (sequence->list rslt)
+                  '(1 1 2 4 5 10 11 23 25 26 54 57 59 122 133 142 147 304 330 351 362 747 806))))
