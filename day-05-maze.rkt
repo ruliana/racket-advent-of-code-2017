fba1b9e43403c909930b046a3adebd56f72b6b1a
Ronie Uliana <ronie.uliana@gmail.com>
2017-12-16 13:27:39 -0200

    Day 05 trampoline :)

--- day-05-maze.rkt
+++ day-05-maze.rkt
@@ -1,24 +1,47 @@
 #lang racket
-(define (update-jump! maze [pos 0] [steps 0])
-  (define (jump maze pos steps)
-    (define jumper (vector-ref maze pos))
-    (vector-set! maze pos (add1 jumper))
-    (update-jump! maze (+ pos jumper) (add1 steps)))
-  (if (>= pos (vector-length maze))
-      steps
-      (jump maze pos steps)))
+
+(define puzzle-a add1)
+(define (puzzle-b n) (if (<= 3 n) (sub1 n) (add1 n))) 
+
+(define update-jumper (make-parameter puzzle-a))
+
+(define (walk-through maze)
+  (define move (update-jumper))
+  (define current (vector-copy maze))
+  (define end-point (vector-length current))
+  (define (stop-condition pos steps)
+    (if (<= end-point pos)
+        (values steps current)
+        (jump pos steps)))
+  (define (jump pos steps)
+    (define jumper (vector-ref current pos))
+    (vector-set! current pos (move jumper))
+    (stop-condition (+ pos jumper) (add1 steps)))
+  (stop-condition 0 0))
 
 (module+ test
   (require rackunit)
   (define maze (vector 0 3 0 1 -3))
-  (define steps (update-jump! maze))
-  (check-equal? steps 5)
-  (check-equal? maze #(2 5 0 1 -2)))
+  (parameterize ([update-jumper puzzle-a])
+    (define-values (steps current) (walk-through maze))
+    (check-equal? steps 5)  
+    (check-equal? current #(2 5 0 1 -2)))
+  (parameterize ([update-jumper puzzle-b])
+    (define-values (steps current) (walk-through maze))
+    (check-equal? steps 10)
+    (check-equal? current #(2 3 2 3 -1))))
 
 (define (count-steps input)
-  (update-jump!
-   (for/vector ([line (in-lines input)])
-     (string->number line))))
+  (let-values ([(steps _)
+                (walk-through
+                 (for/vector ([line (in-lines input)])
+                   (string->number line)))])
+    steps))
 
-(printf "Steps: ~a\n"
-        (call-with-input-file "day-05-input.txt" count-steps #:mode 'text))
+(parameterize ([update-jumper puzzle-a])
+  (printf "Puzzle A: ~a\n"
+          (call-with-input-file "day-05-input.txt" count-steps #:mode 'text)))
+
+(parameterize ([update-jumper puzzle-b])
+  (printf "Puzzle B: ~a\n"
+          (call-with-input-file "day-05-input.txt" count-steps #:mode 'text)))
