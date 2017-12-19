7737d7e7b3b055119868d8176da0006fd359e9c0
Ronie Uliana <ronie.uliana@gmail.com>
2017-12-19 07:48:23 -0200

    WIP

--- day-08-register-reader.rkt
+++ day-08-register-reader.rkt
@@ -8,13 +8,32 @@
 (define (regexp-read in)
   (regexp-read-syntax #f in))
 
+; === Call parse and emit module
 (define (regexp-read-syntax src in)
   (define commands (parse-commands src in))
-  (datum->syntax
-   #f
-   `(module program racket
+  #`(module program racket
       (require "day-08-register.rkt")
-      (inc "a" 2)
-      (largest))))
+      #,@commands
+      (largest)))
 
+; === Parse and build commands
 (define (parse-commands src in)
+  (define matches (regexp-match* #px"\\s*(\\w+)\\s+(inc|dec)\\s+(\\d+)"
+                                 in
+                                 #:match-select rest))
+  (map translate matches))
+
+(define (translate match)
+  (match-define (list reg comm num) match)
+  `(,(->symbol comm)
+    ,(->string reg)
+    ,(->number num)))
+  
+; === Helpers
+(define ->string bytes->string/utf-8)
+         
+(define (->symbol byte-string)
+  (string->symbol (->string byte-string)))
+
+(define (->number byte-string)
+  (string->number (->string byte-string)))
