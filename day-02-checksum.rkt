1d064f4bbd36f7321b95dab0c5948978033b1446
Ronie Uliana <ronie.uliana@gmail.com>
2018-01-07 14:01:19 -0200

    Add readme
    Some notes to remember myself what are the cool parts. Could be useful
    for others too.

--- day-02-checksum.rkt
+++ day-02-checksum.rkt
@@ -2,23 +2,24 @@
 (module+ test
   (require rackunit))
 
-(define (lmax lst) (apply max lst))
-(define (lmin lst) (apply min lst))
+(define (lmax lst) (argmax identity lst))
+(define (lmin lst) (argmin identity lst))
 
 (define (parse text)
   (for/list ([line (in-lines (open-input-string text))])
     (map string->number (regexp-split #px"\\s+" line))))
 
 (define (checksum spreadsheet)
-  (for*/sum ([line spreadsheet])
+  (for/sum ([line spreadsheet])
     (- (lmax line) (lmin line))))
 
 (define (divisibles-on-line line)
-  (define all-pairs (sequence-map (λ (e) (list (lmax e) (lmin e)))
+  ; Sort pairs, largest first, smallest second
+  (define all-pairs (sequence-map (λ (e) (values (lmax e) (lmin e)))
                                   (in-combinations line 2)))
-  (for/list ([elems all-pairs]
-             #:when (zero? (remainder (first elems) (second elems))))
-    (quotient (first elems) (second elems))))
+  (for/list ([(largest smallest) all-pairs]
+             #:when (zero? (remainder largest smallest)))
+    (quotient largest smallest)))
 
 (module+ test
   (check-equal? (divisibles-on-line '(5 9 2 8)) '(4))
